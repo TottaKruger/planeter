b9d6a819aa907a9440fd794124dd969255e6ba23
TottaKruger <106819646+TottaKruger@users.noreply.github.com>
2023-02-24 18:04:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-const searchBarEl = document.querySelector('#search-planet');
-const planetOverlay = document.querySelector('.planet-overlay');
-const prevPlanetBtn = document.querySelector('.pagination-btn--prev');
-const nextPlanetBtn = document.querySelector('.pagination-btn--next');
+const searchBarEl = document.querySelector("#search-planet");
+const planetOverlay = document.querySelector(".planet-overlay");
+const prevPlanetBtn = document.querySelector(".pagination-btn--prev");
+const nextPlanetBtn = document.querySelector(".pagination-btn--next");
+const planetsContainerEl = document.getElementById("planet-container");
+
 let currentPlanetIndex;
 let planetsArray;
 
-drawPlanets();
+load();
 
-async function drawPlanets() {
+searchBarEl.addEventListener("keyup", function () {
+  let input = searchBarEl.value;
+  let matches = [];
+
+  planetsArray.forEach((planet) => {
+    if (planet.name.toLowerCase().includes(input.toLowerCase())) {
+      matches.push(planet);
+    }
+  });
+
+  console.log("matches", matches);
+
+  if (matches.length > 0) {
+    drawPlanets(matches);
+  } else {
+    planetsContainerEl.innerHTML =
+      "Inga matchningar hittades, var god sök igen.";
+  }
+});
+
+async function load() {
+  planetsArray = await loadPlanets();
+  drawPlanets(planetsArray);
+}
+
+async function drawPlanets(planets) {
   try {
-    planetsArray = await loadPlanets();
+    planetsContainerEl.innerHTML = "";
 
-    const container = document.getElementById("planet-container");
-
-    planetsArray.forEach((planet) => {
+    planets.forEach((planet) => {
       const element = document.createElement("div");
       element.classList.add("planet");
       element.classList.add(planet.name.toLowerCase());
       element.addEventListener("click", () => openPlanetOverlay(planet));
-      container.appendChild(element);
+      planetsContainerEl.appendChild(element);
     });
   } catch (error) {
     console.log(error);
   }
 }
-
-
 
 async function loadPlanets() {
   const res = await fetch("https://majazocom.github.io/Data/solaris.json");
@@ -33,46 +56,26 @@ async function loadPlanets() {
   return planets;
 }
 
-searchBarEl.addEventListener('keyup', function () {
-    let input = searchBarEl.value;
-    let matches = [];
-
-    planetsArray.forEach(planet => {
-         if (planet.name.toLowerCase().includes(input.toLowerCase())) {
-            matches.push(planet);
-        }
-    });
-    if (matches.length > 0) {
-        loadPlanets(matches);
-    } else {
-        planetsContainerEL.innerHTML = "Inga matchningar hittades, var god sök igen."
-    }
-});
-
 function openPlanetOverlay(planet) {
-    currentPlanetIndex = planetsArray.indexOf(planet);
-    console.log('index of planet in list: ', currentPlanetIndex);
-    document.querySelector('.planet-name').innerHTML = planet.name;
-    document.querySelector('.planet-desc').innerHTML = planet.desc;
-    document.querySelector('.planet-orbitalPeriod').innerHTML = planet.orbitalPeriod;
-    document.querySelector('.planet-moons').innerHTML = planet.moons;
-    document.querySelector('.planet-type').innerHTML = planet.type;
-    document.querySelector('.planet-latinName').innerHTML = planet.latinName;
-    document.querySelector('.planet-rotation').innerHTML = planet.rotation;
-    document.querySelector('.planet-circumference').innerHTML = planet.circumference;
-    document.querySelector('.planet-temp').innerHTML = planet.temp;
-    document.querySelector('.planet-distance').innerHTML = planet.distance;
-    planetOverlay.style.display = 'block';
+  currentPlanetIndex = planetsArray.indexOf(planet);
+  console.log("index of planet in list: ", currentPlanetIndex);
+  document.querySelector(".planet-name").innerHTML = planet.name;
+  document.querySelector(".planet-desc").innerHTML = planet.desc;
+  document.querySelector(".planet-orbitalPeriod").innerHTML = planet.orbitalPeriod;
+  document.querySelector(".planet-moons").innerHTML = planet.moons;
+  document.querySelector(".planet-type").innerHTML = planet.type;
+  document.querySelector(".planet-latinName").innerHTML = planet.latinName;
+  document.querySelector(".planet-rotation").innerHTML = planet.rotation;
+  document.querySelector(".planet-circumference").innerHTML = planet.circumference;
+  document.querySelector(".planet-temp").innerHTML = planet.temp;
+  document.querySelector(".planet-distance").innerHTML = planet.distance;
+  planetOverlay.style.display = "block";
+}
 
-};
-
-
-
-
-prevPlanetBtn.addEventListener('click', () => {
-    openPlanetOverlay(planetsArray[currentPlanetIndex - 1]);
+prevPlanetBtn.addEventListener("click", () => {
+  openPlanetOverlay(planetsArray[currentPlanetIndex - 1]);
 });
 
-nextPlanetBtn.addEventListener('click', () => {
-    openPlanetOverlay(planetsArray[currentPlanetIndex + 1]);
+nextPlanetBtn.addEventListener("click", () => {
+  openPlanetOverlay(planetsArray[currentPlanetIndex + 1]);
 });
